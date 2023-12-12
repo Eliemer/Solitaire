3b9709c0bffbbae452fcc158658013993c311ea5
Eliemer <eliemer619@gmail.com>
2023-12-11 20:22:36 -0500

    init commit and basic domain

---
+++ Card.Domain.fs
@@ -0,0 +1,114 @@
+﻿namespace Solitaire.Domain
+
+module Cards =
+    open Microsoft.FSharp.Reflection
+
+    [<NoComparison>]
+    [<Struct>]
+    type Color =
+        | Red
+        | Black
+
+    [<Struct>]
+    [<NoComparison>]
+    type Suit =
+        | Clubs
+        | Diamonds
+        | Hearts
+        | Spades
+
+        member this.Color =
+            match this with
+            | Spades
+            | Clubs -> Black
+            | Diamonds
+            | Hearts -> Red
+
+    [<Struct>]
+    type Rank =
+        | Ace
+        | Two
+        | Three
+        | Four
+        | Five
+        | Six
+        | Seven
+        | Eight
+        | Nine
+        | Ten
+        | Jack
+        | Queen
+        | King
+
+    type Card = { Suit : Suit; Rank : Rank }
+
+    type Spot =
+        // Where all 52 cards come from
+        | Deck
+        // The play area piles
+        | Pile
+        // Where the suits are built up
+        | Foundation of Suit
+
+    // The first element of the list is the top of the stack
+    type Stack = { Cards : Card list; Spot : Spot }
+
+    let StandardDeck =
+        {
+            Stack.Spot = Deck
+            Cards =
+                [
+                    { Suit = Clubs; Rank = Ace }
+                    { Suit = Diamonds; Rank = Ace }
+                    { Suit = Hearts; Rank = Ace }
+                    { Suit = Spades; Rank = Ace }
+                    { Suit = Clubs; Rank = Two }
+                    { Suit = Diamonds; Rank = Two }
+                    { Suit = Hearts; Rank = Two }
+                    { Suit = Spades; Rank = Two }
+                    { Suit = Clubs; Rank = Three }
+                    { Suit = Diamonds; Rank = Three }
+                    { Suit = Hearts; Rank = Three }
+                    { Suit = Spades; Rank = Three }
+                    { Suit = Clubs; Rank = Four }
+                    { Suit = Diamonds; Rank = Four }
+                    { Suit = Hearts; Rank = Four }
+                    { Suit = Spades; Rank = Four }
+                    { Suit = Clubs; Rank = Five }
+                    { Suit = Diamonds; Rank = Five }
+                    { Suit = Hearts; Rank = Five }
+                    { Suit = Spades; Rank = Five }
+                    { Suit = Clubs; Rank = Six }
+                    { Suit = Diamonds; Rank = Six }
+                    { Suit = Hearts; Rank = Six }
+                    { Suit = Spades; Rank = Six }
+                    { Suit = Clubs; Rank = Seven }
+                    { Suit = Diamonds; Rank = Seven }
+                    { Suit = Hearts; Rank = Seven }
+                    { Suit = Spades; Rank = Seven }
+                    { Suit = Clubs; Rank = Eight }
+                    { Suit = Diamonds; Rank = Eight }
+                    { Suit = Hearts; Rank = Eight }
+                    { Suit = Spades; Rank = Eight }
+                    { Suit = Clubs; Rank = Nine }
+                    { Suit = Diamonds; Rank = Nine }
+                    { Suit = Hearts; Rank = Nine }
+                    { Suit = Spades; Rank = Nine }
+                    { Suit = Clubs; Rank = Ten }
+                    { Suit = Diamonds; Rank = Ten }
+                    { Suit = Hearts; Rank = Ten }
+                    { Suit = Spades; Rank = Ten }
+                    { Suit = Clubs; Rank = Jack }
+                    { Suit = Diamonds; Rank = Jack }
+                    { Suit = Hearts; Rank = Jack }
+                    { Suit = Spades; Rank = Jack }
+                    { Suit = Clubs; Rank = Queen }
+                    { Suit = Diamonds; Rank = Queen }
+                    { Suit = Hearts; Rank = Queen }
+                    { Suit = Spades; Rank = Queen }
+                    { Suit = Clubs; Rank = King }
+                    { Suit = Diamonds; Rank = King }
+                    { Suit = Hearts; Rank = King }
+                    { Suit = Spades; Rank = King }
+                ]
+        }
---
+++ Card.Logic.fs
@@ -0,0 +1,110 @@
+namespace Solitaire.Domain
+
+open Solitaire.Domain.Cards
+
+module Logic =
+    type StackError = | CantAddToStack
+
+    module Suit =
+        let sameColor (a : Suit) (b : Suit) = a.Color = b.Color
+
+    module Rank =
+        [<Literal>]
+        let Ace = "Ace"
+
+        [<Literal>]
+        let Two = "Two"
+
+        [<Literal>]
+        let Three = "Three"
+
+        [<Literal>]
+        let Four = "Four"
+
+        [<Literal>]
+        let Five = "Five"
+
+        [<Literal>]
+        let Six = "Six"
+
+        [<Literal>]
+        let Seven = "Seven"
+
+        [<Literal>]
+        let Eight = "Eight"
+
+        [<Literal>]
+        let Nine = "Nine"
+
+        [<Literal>]
+        let Ten = "Ten"
+
+        [<Literal>]
+        let Jack = "Jack"
+
+        [<Literal>]
+        let Queen = "Queen"
+
+        [<Literal>]
+        let King = "King"
+
+        let toString (r : Rank) : string =
+            match r with
+            | Rank.Ace -> Ace
+            | Rank.Two -> Two
+            | Rank.Three -> Three
+            | Rank.Four -> Four
+            | Rank.Five -> Five
+            | Rank.Six -> Six
+            | Rank.Seven -> Seven
+            | Rank.Eight -> Eight
+            | Rank.Nine -> Nine
+            | Rank.Ten -> Ten
+            | Rank.Jack -> Jack
+            | Rank.Queen -> Queen
+            | Rank.King -> King
+
+        let parse (s : string) : Result<Rank, string> =
+            match s.ToLowerInvariant() with
+            | Ace -> Ok Rank.Ace
+            | Two -> Ok Rank.Two
+            | Three -> Ok Rank.Three
+            | Four -> Ok Rank.Four
+            | Five -> Ok Rank.Five
+            | Six -> Ok Rank.Six
+            | Seven -> Ok Rank.Seven
+            | Eight -> Ok Rank.Eight
+            | Nine -> Ok Rank.Nine
+            | Ten -> Ok Rank.Ten
+            | Jack -> Ok Rank.Jack
+            | Queen -> Ok Rank.Queen
+            | King -> Ok Rank.King
+            | e -> Error $"{e} is not a valid card rank"
+
+    module Stack =
+
+        let addToStack (c : Card) (stack : Stack) : Result<Stack, StackError> =
+            match stack with
+            | { Cards = _; Spot = Deck } -> Error CantAddToStack
+            | { Cards = cards; Spot = Pile } ->
+                match cards with
+                | [] -> Ok { stack with Cards = [ c ] }
+                | top :: rest ->
+                    if Suit.sameColor top.Suit c.Suit || top.Rank <= c.Rank then
+                        Error CantAddToStack
+                    else
+                        Ok { stack with Cards = c :: top :: rest }
+            | {
+                  Cards = cards
+                  Spot = Foundation targetSuit
+              } ->
+                if c.Suit <> targetSuit then
+                    Error CantAddToStack
+                else
+                    match cards with
+                    | [] -> Ok { stack with Cards = [ c ] }
+                    | top :: rest ->
+                        if c.Rank < top.Rank then
+                            Ok { stack with Cards = c :: top :: rest }
+                        else
+                            Error CantAddToStack
